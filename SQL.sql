5acbdd953cd8afd274f1cf6b5f357c8be14930d5
Artem <artemsyuzev02@yandex.ru>
2021-12-07 15:23:46 +0300

    first commit

---
+++ SQL.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+  ` model avto`.модель
+FROM клиенты
+  INNER JOIN ` model avto`
+    ON клиенты.id_model = ` model avto`.id_model
+  INNER JOIN `коробка передач`
+    ON ` model avto`.id_korobka = `коробка передач`.id_korobka
+  INNER JOIN цвет
+    ON ` model avto`.id_color = цвет.id_color
+WHERE ` model avto`.модель = 'LADA'
